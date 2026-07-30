468893e5be409894c00852417122374eb0e535c4
Peter Hawkins <phawkins@google.com>
2026-07-29 17:24:50 -0700

    Fix issue where hypothesis tests that specified suppress_health_check overrode the existing health checks.
    This caused issues where checks that we suppress by default in hypothesis_test_util.setup_hypothesis were triggering in IC:
    
    ```
    ________ VectorSubcoreTestWithTCTiling.test_block_spec_untiled_slicing _________
    [gw0] linux -- Python 3.12.13 /usr/bin/python3.12
    tests/pallas/tpu_sparsecore_pallas_test.py:521: in test_block_spec_untiled_slicing
        @hp.given(hps.data())
                     ^^^^^^^
    E   hypothesis.errors.FailedHealthCheck: The method VectorSubcoreTest.test_block_spec_untiled_slicing was called from multiple different executors. This may lead to flaky tests and nonreproducible errors when replaying from database.
    E
    E   Unlike most health checks, HealthCheck.differing_executors warns about a correctness issue with your test. We therefore recommend fixing the underlying issue, rather than suppressing this health check. However, if you are confident this health check can be safely disabled, you can do so with @settings(suppress_health_check=[HealthCheck.differing_executors]). See https://hypothesis.readthedocs.io/en/latest/reference/api.html#hypothesis.HealthCheck for details.
    ```
    
    PiperOrigin-RevId: 956173688

--- tests/pallas/tpu_splash_attention_kernel_test.py
+++ tests/pallas/tpu_splash_attention_kernel_test.py
@@ -337,7 +337,10 @@ class SplashAttentionTest(PallasBaseTest):
       is_dynamic_mask=(False, True),
   )
   @hp.given(hps.data())
-  @hp.settings(suppress_health_check=[hp.HealthCheck.filter_too_much])
+  @hp.settings(
+      suppress_health_check=hp.settings.default.suppress_health_check
+      + (hp.HealthCheck.filter_too_much,)
+  )
   def test_splash_attention(self, is_mqa, is_segmented, is_dynamic_mask, data):
     seed = data.draw(seed_strategy())
     key = random.key(seed)
@@ -415,7 +418,10 @@ class SplashAttentionTest(PallasBaseTest):
       is_dynamic_mask=(False, True),
   )
   @hp.given(hps.data())
-  @hp.settings(suppress_health_check=[hp.HealthCheck.filter_too_much])
+  @hp.settings(
+      suppress_health_check=hp.settings.default.suppress_health_check
+      + (hp.HealthCheck.filter_too_much,)
+  )
   def test_splash_attention_fwd(
       self, is_mqa, is_segmented, is_dynamic_mask, data
   ):
@@ -498,7 +504,10 @@ class SplashAttentionTest(PallasBaseTest):
       is_segmented=(False, True),
   )
   @hp.given(hps.data())
-  @hp.settings(suppress_health_check=[hp.HealthCheck.filter_too_much])
+  @hp.settings(
+      suppress_health_check=hp.settings.default.suppress_health_check
+      + (hp.HealthCheck.filter_too_much,)
+  )
   def test_splash_attention_custom_bwd(self, is_segmented, data):
     seed = data.draw(seed_strategy(), label="seed")
     key = random.key(1 + seed)
@@ -594,7 +603,10 @@ class SplashAttentionTest(PallasBaseTest):
       use_sinks=(False, True),
   )
   @hp.given(hps.data())
-  @hp.settings(suppress_health_check=[hp.HealthCheck.filter_too_much])
+  @hp.settings(
+      suppress_health_check=hp.settings.default.suppress_health_check
+      + (hp.HealthCheck.filter_too_much,)
+  )
   def test_splash_attention_bwd(
       self,
       is_mqa,
